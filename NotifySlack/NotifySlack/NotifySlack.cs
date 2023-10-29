d5abc2e96870cf00cac6c6cee39941c6bb189877
Mathijs <mathijskroese@gmail.com>
2023-10-29 11:36:06 +0100

    first db connection try

--- NotifySlack/NotifySlack/NotifySlack.cs
+++ NotifySlack/NotifySlack/NotifySlack.cs
@@ -10,6 +10,9 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using System.Text;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Windows.Markup;
 
 namespace NotifySlack
 {
@@ -25,41 +28,64 @@ namespace NotifySlack
             var body = await new StreamReader(req.Body).ReadToEndAsync();
             var content = JObject.Parse(body);
 
-            if (content != null)
-                await SendToSlack(content);
+            if (content == null)
+                return new BadRequestObjectResult("No body found");
 
-            string responseMessage = content != null ? "Slack has been notified." : "No body found";
-
-            return new OkObjectResult(responseMessage);
-        }
-
-        private static async Task<string> SendToSlack(JToken token)
-        {
-            string message = BuildMessage(token);
-
-            string slackWebhook = "https://hooks.slack.com/services/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";
-
-            using (HttpClient client = new HttpClient())
+            string slackMessage = "";
+            try
             {
-                var payload = new StringContent(JsonConvert.SerializeObject(new { text = message }), Encoding.UTF8, "application/json");
-
-                var response = await client.PostAsync(slackWebhook, payload);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new Exception($"Failed to send message to Slack: {response.StatusCode}");
-                }
-
-                return "Slack has been notified!";
+                List<string> values = ExtractValues(content);
+                slackMessage = $"A commit has been pushed to [ {values[0]} ] by [ {values[1]} ] on [ {values[2]} ] with the following message: \n[ {values[3]} ]";
+                await SendToSlack(slackMessage);
+                await UpdateDatabase(values);
+                return new OkObjectResult("Slack has been notified");
+            }
+            catch (Exception e)
+            { 
+                log.LogError(e.Message);
+                return new BadRequestObjectResult($"Error: {e.Message}");
             }
         }
 
-        private static string BuildMessage(JToken token)
+        private static async Task<string> SendToSlack(string message)
         {
-            string username = "";
-            string repository = "";
-            string timestamp = "";
-            string message = "";
+
+            using (HttpClient client = new HttpClient())
+            {
+                string slackWebhook = "https://hooks.slack.com/services/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";
+                var payload = new StringContent(JsonConvert.SerializeObject(new { text = message }), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(slackWebhook, payload);
+
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception($"Failed to send message to Slack: {response.StatusCode}");
+
+                return "Slack has been notified!";
+            }
+
+        }
+
+        private static async Task UpdateDatabase(List<string> values)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("notify-slack-connection", EnvironmentVariableTarget.Process);
+            var responseResult = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var db = connection.Database;
+                var query = $"INSERT INTO {db} VALUES('{values[4]}, {values[0]}, {values[1]}, {values[2]}, {values[3]}')";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    responseResult = await command.ExecuteNonQueryAsync();
+                }
+                connection.Close();
+            }
+        }
+
+        private static List<string> ExtractValues(JToken token)
+        {
+            List<string> values = new List<string>();
 
             foreach (var property in token.Children<JProperty>())
             {
@@ -69,17 +95,18 @@ namespace NotifySlack
                 string currentName = property.Name.ToString();
                 switch (currentName)
                 {
+                    case "repository":
+                        values.Add(property.Value["name"].ToString());
+                        break;
+
                     case "pusher":
-                        username = property.Value["name"].ToString();
+                        values.Add(property.Value["name"].ToString());
                         break;
 
                     case "head_commit":
-                        timestamp = property.Value["timestamp"].ToString();
-                        message = property.Value["message"].ToString();
-                        break;
-
-                    case "repository":
-                        repository = property.Value["name"].ToString();
+                        values.Add(property.Value["timestamp"].ToString());
+                        values.Add(property.Value["message"].ToString());
+                        values.Add(property.Value["id"].ToString());
                         break;
 
                     default:
@@ -87,9 +114,10 @@ namespace NotifySlack
                 }
 
                 if (property.Value.Type == JTokenType.Object)
-                    BuildMessage(property.Value);
+                    ExtractValues(property.Value);
             }
-            return $"A commit has been pushed to [ {repository} ] by [ {username} ] on [ {timestamp} ] with the following message: \n[ {message} ]";
+
+            return values;
         }
     }
 }
