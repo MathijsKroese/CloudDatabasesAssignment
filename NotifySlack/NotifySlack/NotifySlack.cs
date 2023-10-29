c854513226c65da51e5ff960f2a41e9352d1d1e1
Mathijs <mathijskroese@gmail.com>
2023-10-29 02:23:51 +0100

    small change

--- NotifySlack/NotifySlack/NotifySlack.cs
+++ NotifySlack/NotifySlack/NotifySlack.cs
@@ -28,15 +28,7 @@ namespace NotifySlack
             if (content != null)
                 await SendToSlack(content);
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a more personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = content != null ? "Slack has been notified." : "No body found";
 
             return new OkObjectResult(responseMessage);
         }
